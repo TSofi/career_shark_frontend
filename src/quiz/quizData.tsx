a17482180bc18625cce897b10dcdc679c078e96f
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2024-10-12 23:15:08 +0200

    still quiz form

--- src/quiz/quizData.tsx
+++ src/quiz/quizData.tsx
@@ -3,29 +3,26 @@ import axios from "axios";
 import { GetQuizDto } from "../api/dto/quiz_dto";
 import { useApi } from "../api/ApiProvider";
 
-const useLoans = () => {
+const useQuizData = () => {
   const [data, setData] = useState<GetQuizDto | undefined>(undefined);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const apiClient = useApi();
 
-  const fetchLoans = useCallback(
-    async (page: number, rowsPerPage: number) => {
-      setLoading(true);
-      setError(null);
-      try {
-        const response = await apiClient.getWelcomeQuiz();
-        setData(response.data);
-      } catch (err) {
-        setError("Failed to fetch loans");
-      } finally {
-        setLoading(false);
-      }
-    },
-    [apiClient]
-  );
+  const fetchQuizData = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await apiClient.getWelcomeQuiz();
+      setData(response.data);
+    } catch (err) {
+      setError("Failed to fetch loans");
+    } finally {
+      setLoading(false);
+    }
+  }, [apiClient]);
 
-  return { data, fetchLoans, loading, error };
+  return { data, fetchQuizData, loading, error };
 };
 
-export default useLoans;
+export default useQuizData;
